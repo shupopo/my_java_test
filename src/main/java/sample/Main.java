6e16fbbccf4b40d4bfb474fdaa392d6d1252248b
Fumiya <Fumiya@Fumiya-MacBook-Pro.local>
2017-05-01 01:19:35 +0900

    Add calc calss

---
+++ src/main/java/sample/Main.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class Main {
+    public static void main(String[] args){
+        Calculator calc = new Calculator();
+        System.out.println(calc.add(10, 20));
+        System.out.println(calc.subtract(5, 3));
+    }
+}
